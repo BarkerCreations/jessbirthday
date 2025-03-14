2fc44ad67219efb55064055a253f13e2fd8056ad
Chris Barker <tech@string-tms.com>
2025-03-14 23:29:19 +0100

    Padding bottom

--- src/routes/choices/+page.svelte
+++ src/routes/choices/+page.svelte
@@ -97,7 +97,7 @@
         display: flex;
         flex-direction: column;
         background: rgb(40, 40, 40);
-        height: 100vh;
+        height: calc(100vh + 80px);
         width: 100vw;
     }
 
